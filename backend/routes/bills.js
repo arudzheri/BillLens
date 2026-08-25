af36c8e12a5707b46b74a99b50f1b4a322a4ac83
nikolpavlova <nikolpavlovaofficial@gmail.com>
2026-08-25 11:27:44 +0300

    Add bills data structure to bills.js
    Added a JSON object containing a list of bills with details such as billId, shortTitle, currentHouse, and currentStage.

--- backend/routes/bills.js
+++ backend/routes/bills.js
@@ -1,1 +1,590 @@
-
+{
+    "items": [
+        {
+            "billId": 3973,
+            "shortTitle": "A34 Slip Road Safety (East Ilsley and Beedon) Bill",
+            "formerShortTitle": null,
+            "currentHouse": "Commons",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2025-09-16T17:08:18.2184786",
+            "billWithdrawn": "2025-09-15T00:00:00",
+            "isDefeated": false,
+            "billTypeId": 5,
+            "introducedSessionId": 39,
+            "includedSessionIds": [
+                39
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 19929,
+                "stageId": 7,
+                "sessionId": 39,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Commons",
+                "stageSittings": [
+                    {
+                        "id": 17032,
+                        "stageId": 7,
+                        "billStageId": 19929,
+                        "billId": 3973,
+                        "date": "2025-07-11T00:00:00"
+                    }
+                ],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 2818,
+            "shortTitle": "Abolition of Business Rates Bill",
+            "formerShortTitle": null,
+            "currentHouse": "Commons",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2021-05-04T10:42:08.0579851",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 5,
+            "introducedSessionId": 35,
+            "includedSessionIds": [
+                35
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 12655,
+                "stageId": 7,
+                "sessionId": 35,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Commons",
+                "stageSittings": [],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 2302,
+            "shortTitle": "Abortion Bill",
+            "formerShortTitle": null,
+            "currentHouse": "Commons",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2019-09-17T16:22:10",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 5,
+            "introducedSessionId": 30,
+            "includedSessionIds": [
+                30
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 15039,
+                "stageId": 7,
+                "sessionId": 30,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Commons",
+                "stageSittings": [],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 2556,
+            "shortTitle": "Abortion Bill [HL]",
+            "formerShortTitle": null,
+            "currentHouse": "Lords",
+            "originatingHouse": "Lords",
+            "lastUpdate": "2021-05-05T11:05:17.4984479",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 2,
+            "introducedSessionId": 35,
+            "includedSessionIds": [
+                35
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 11662,
+                "stageId": 2,
+                "sessionId": 35,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Lords",
+                "stageSittings": [],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 2743,
+            "shortTitle": "Abortion (Cleft Lip, Cleft Palate and Clubfoot) Bill",
+            "formerShortTitle": null,
+            "currentHouse": "Commons",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2021-05-04T10:55:22.6940381",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 8,
+            "introducedSessionId": 35,
+            "includedSessionIds": [
+                35
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 12196,
+                "stageId": 7,
+                "sessionId": 35,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Commons",
+                "stageSittings": [],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 1790,
+            "shortTitle": "Abortion (Disability Equality) Bill [HL]",
+            "formerShortTitle": null,
+            "currentHouse": "Lords",
+            "originatingHouse": "Lords",
+            "lastUpdate": "2017-05-02T10:36:51",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 2,
+            "introducedSessionId": 29,
+            "includedSessionIds": [
+                29
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 9209,
+                "stageId": 4,
+                "sessionId": 29,
+                "description": "Report stage",
+                "abbreviation": "RS",
+                "house": "Lords",
+                "stageSittings": [
+                    {
+                        "id": 8494,
+                        "stageId": 4,
+                        "billStageId": 9209,
+                        "billId": 1790,
+                        "date": "2017-02-24T00:00:00"
+                    }
+                ],
+                "sortOrder": 4
+            }
+        },
+        {
+            "billId": 2043,
+            "shortTitle": "Abortion (Disability Equality) Bill [HL]",
+            "formerShortTitle": null,
+            "currentHouse": "Lords",
+            "originatingHouse": "Lords",
+            "lastUpdate": "2019-10-09T10:23:08",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 2,
+            "introducedSessionId": 30,
+            "includedSessionIds": [
+                30
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 14787,
+                "stageId": 2,
+                "sessionId": 30,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Lords",
+                "stageSittings": [],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 2028,
+            "shortTitle": "Abortion (Foetus Protection) Bill [HL]",
+            "formerShortTitle": null,
+            "currentHouse": "Lords",
+            "originatingHouse": "Lords",
+            "lastUpdate": "2019-10-09T10:24:33",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 2,
+            "introducedSessionId": 30,
+            "includedSessionIds": [
+                30
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 14772,
+                "stageId": 2,
+                "sessionId": 30,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Lords",
+                "stageSittings": [],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 3554,
+            "shortTitle": "Abortion (Gestational Time Limit Reduction) Bill [HL]",
+            "formerShortTitle": null,
+            "currentHouse": "Lords",
+            "originatingHouse": "Lords",
+            "lastUpdate": "2024-05-29T11:30:05.9068571",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 2,
+            "introducedSessionId": 38,
+            "includedSessionIds": [
+                38
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 18160,
+                "stageId": 1,
+                "sessionId": 38,
+                "description": "1st reading",
+                "abbreviation": "1R",
+                "house": "Lords",
+                "stageSittings": [
+                    {
+                        "id": 15147,
+                        "stageId": 1,
+                        "billStageId": 18160,
+                        "billId": 3554,
+                        "date": "2023-12-07T00:00:00"
+                    }
+                ],
+                "sortOrder": 1
+            }
+        },
+        {
+            "billId": 1514,
+            "shortTitle": "Abortion (Sex-Selection) Bill",
+            "formerShortTitle": null,
+            "currentHouse": "Commons",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2015-03-30T11:16:23",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 5,
+            "introducedSessionId": 27,
+            "includedSessionIds": [
+                27
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 14298,
+                "stageId": 7,
+                "sessionId": 27,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Commons",
+                "stageSittings": [],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 1404,
+            "shortTitle": "Abortion Act 1967 (Amendment) Bill [HL]",
+            "formerShortTitle": null,
+            "currentHouse": "Lords",
+            "originatingHouse": "Lords",
+            "lastUpdate": "2014-06-13T09:17:23",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 2,
+            "introducedSessionId": 27,
+            "includedSessionIds": [
+                27
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 14192,
+                "stageId": 2,
+                "sessionId": 27,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Lords",
+                "stageSittings": [],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 1159,
+            "shortTitle": "Abortion Statistics Bill",
+            "formerShortTitle": null,
+            "currentHouse": "Commons",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2013-04-26T09:46:20",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 5,
+            "introducedSessionId": 25,
+            "includedSessionIds": [
+                25
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 13958,
+                "stageId": 7,
+                "sessionId": 25,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Commons",
+                "stageSittings": [],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 3785,
+            "shortTitle": "Absent Voting (Elections in Scotland and Wales) Act 2025",
+            "formerShortTitle": null,
+            "currentHouse": "Unassigned",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2025-10-31T16:45:37.3019749",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 7,
+            "introducedSessionId": 39,
+            "includedSessionIds": [
+                39
+            ],
+            "isAct": true,
+            "currentStage": {
+                "id": 20255,
+                "stageId": 11,
+                "sessionId": 39,
+                "description": "Royal Assent",
+                "abbreviation": "RA",
+                "house": "Unassigned",
+                "stageSittings": [
+                    {
+                        "id": 17473,
+                        "stageId": 11,
+                        "billStageId": 20255,
+                        "billId": 3785,
+                        "date": "2025-10-27T00:00:00"
+                    }
+                ],
+                "sortOrder": 11
+            }
+        },
+        {
+            "billId": 3049,
+            "shortTitle": "Abuse of Public-facing Workers (Offences) Bill",
+            "formerShortTitle": null,
+            "currentHouse": "Commons",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2022-04-29T11:13:43.2350902",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 5,
+            "introducedSessionId": 36,
+            "includedSessionIds": [
+                36
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 15754,
+                "stageId": 7,
+                "sessionId": 36,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Commons",
+                "stageSittings": [
+                    {
+                        "id": 12302,
+                        "stageId": 7,
+                        "billStageId": 15754,
+                        "billId": 3049,
+                        "date": "2022-05-06T00:00:00"
+                    }
+                ],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 642,
+            "shortTitle": "Academies Act 2010",
+            "formerShortTitle": null,
+            "currentHouse": "Unassigned",
+            "originatingHouse": "Lords",
+            "lastUpdate": "2021-03-18T09:27:41.123269",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 1,
+            "introducedSessionId": 24,
+            "includedSessionIds": [
+                24
+            ],
+            "isAct": true,
+            "currentStage": {
+                "id": 3705,
+                "stageId": 11,
+                "sessionId": 24,
+                "description": "Royal Assent",
+                "abbreviation": "RA",
+                "house": "Unassigned",
+                "stageSittings": [
+                    {
+                        "id": 3400,
+                        "stageId": 11,
+                        "billStageId": 3705,
+                        "billId": 642,
+                        "date": "2010-07-27T00:00:00"
+                    }
+                ],
+                "sortOrder": 13
+            }
+        },
+        {
+            "billId": 2215,
+            "shortTitle": "Access to Banking Services Bill",
+            "formerShortTitle": null,
+            "currentHouse": "Commons",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2019-09-17T16:22:19",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 5,
+            "introducedSessionId": 30,
+            "includedSessionIds": [
+                30
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 14955,
+                "stageId": 7,
+                "sessionId": 30,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Commons",
+                "stageSittings": [],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 26,
+            "shortTitle": "Access To Contraceptive Services Bill",
+            "formerShortTitle": null,
+            "currentHouse": "Commons",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2024-05-10T11:40:52.5925826",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 5,
+            "introducedSessionId": 20,
+            "includedSessionIds": [
+                20
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 12944,
+                "stageId": 7,
+                "sessionId": 20,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Commons",
+                "stageSittings": [],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 3239,
+            "shortTitle": "Access to Elected Office Fund (Report) Bill",
+            "formerShortTitle": null,
+            "currentHouse": "Commons",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2023-10-30T15:47:58.8605171",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 8,
+            "introducedSessionId": 37,
+            "includedSessionIds": [
+                37
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 16676,
+                "stageId": 6,
+                "sessionId": 37,
+                "description": "1st reading",
+                "abbreviation": "1R",
+                "house": "Commons",
+                "stageSittings": [
+                    {
+                        "id": 13395,
+                        "stageId": 6,
+                        "billStageId": 16676,
+                        "billId": 3239,
+                        "date": "2022-06-20T00:00:00"
+                    }
+                ],
+                "sortOrder": 1
+            }
+        },
+        {
+            "billId": 4273,
+            "shortTitle": "Access to Elected Office Fund (Report) Bill",
+            "formerShortTitle": null,
+            "currentHouse": "Commons",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2026-07-16T12:59:49.1841232",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 8,
+            "introducedSessionId": 40,
+            "includedSessionIds": [
+                40
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 21121,
+                "stageId": 7,
+                "sessionId": 40,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Commons",
+                "stageSittings": [
+                    {
+                        "id": 18524,
+                        "stageId": 7,
+                        "billStageId": 21121,
+                        "billId": 4273,
+                        "date": "2027-01-15T00:00:00"
+                    }
+                ],
+                "sortOrder": 2
+            }
+        },
+        {
+            "billId": 2239,
+            "shortTitle": "Access to Fertility Services Bill",
+            "formerShortTitle": null,
+            "currentHouse": "Commons",
+            "originatingHouse": "Commons",
+            "lastUpdate": "2019-09-17T16:22:27",
+            "billWithdrawn": null,
+            "isDefeated": false,
+            "billTypeId": 5,
+            "introducedSessionId": 30,
+            "includedSessionIds": [
+                30
+            ],
+            "isAct": false,
+            "currentStage": {
+                "id": 14978,
+                "stageId": 7,
+                "sessionId": 30,
+                "description": "2nd reading",
+                "abbreviation": "2R",
+                "house": "Commons",
+                "stageSittings": [],
+                "sortOrder": 2
+            }
+        }
+    ],
+    "totalResults": 4035,
+    "itemsPerPage": 20
+}
