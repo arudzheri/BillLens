141efb5589f1ec0edcfd5de46f505ae78e1cdcb3
w1996507 <143714867+arudzheri@users.noreply.github.com>
2026-08-30 21:50:54 +0100

    Add imports and __all__ definition in __init__.py

---
+++ apps/web/legislation/__init__.py
@@ -0,0 +1,17 @@
+from .schemas import (
+    LegislationDetailResponse,
+    LegislationSearchRequest,
+    LegislationSearchResponse,
+    LegislationSummary,
+)
+
+from .service import LegislationService
+
+
+__all__ = [
+    "LegislationDetailResponse",
+    "LegislationSearchRequest",
+    "LegislationSearchResponse",
+    "LegislationSummary",
+    "LegislationService",
+]
