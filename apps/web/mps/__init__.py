bfa77d8c61b797f9911ea599655f3777995f845f
w1996507 <143714867+arudzheri@users.noreply.github.com>
2026-08-30 21:49:01 +0100

    Add imports and define __all__ in __init__.py

---
+++ apps/web/mps/__init__.py
@@ -0,0 +1,15 @@
+from .schemas import (
+    MPSearchRequest,
+    MPSearchResponse,
+    MPSummary,
+)
+
+from .service import MPsService
+
+
+__all__ = [
+    "MPSearchRequest",
+    "MPSearchResponse",
+    "MPSummary",
+    "MPsService",
+]
