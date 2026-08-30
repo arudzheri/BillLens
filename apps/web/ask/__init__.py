895c4633a311f4b2ea1b8b247913a74065e4c8a9
w1996507 <143714867+arudzheri@users.noreply.github.com>
2026-08-30 21:44:52 +0100

    Import schemas and service in __init__.py

---
+++ apps/web/ask/__init__.py
@@ -0,0 +1,15 @@
+from .schemas import (
+    AskRequest,
+    AskResponse,
+    AskSource,
+)
+
+from .service import AskService
+
+
+__all__ = [
+    "AskRequest",
+    "AskResponse",
+    "AskService",
+    "AskSource",
+]
