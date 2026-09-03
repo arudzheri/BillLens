ebc7f150f1a7d9d80f0fcbb87d479fb06dcf4318
nikolpavlova <nikolpavlovaofficial@gmail.com>
2026-09-03 21:05:51 +0000

    New

--- apps/api/__init__.py
+++ apps/api/__init__.py
@@ -1,14 +1,1 @@
-from .ask_service import AskService
-from .bills_service import BillsService
-from .dashboard_service import DashboardService
-from .legislation_service import LegislationService
-from .mps_service import MPsService
-
-
-__all__ = [
-    "AskService",
-    "BillsService",
-    "DashboardService",
-    "LegislationService",
-    "MPsService",
-]
+"""BillLens API package."""
